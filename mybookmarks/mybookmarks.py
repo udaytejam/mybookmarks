4f35a5c24471dfe89a822ddb7c25b370999fd883
uday <udayteja23@gmail.com>
2018-03-18 00:54:25 +0530

    Allow add method to append url to a global 'bookmarks' variable for persistence.

--- mybookmarks/mybookmarks.py
+++ mybookmarks/mybookmarks.py
@@ -1,25 +1,31 @@
-from flask import Flask, render_template, url_for
+from datetime import datetime
+from flask import Flask, render_template, url_for, request, redirect
+from logging import DEBUG
 
 app = Flask(__name__)
+app.logger.setLevel(DEBUG)
 
-class User:
-    def __init__(self, first, last):
-        self.firstname = first
-        self.lastname = last
-
-    def __str__(self):
-        return "{} {}".format(self.firstname, self.lastname)
-
-    def return_pee(self, x):
-        return "Peep :) " + x
+bookmarks = []
+#using global bookmarks as a variable to store data. Database to follow.
+def store_bookmark(url):
+    bookmarks.append(dict(
+        url = url,
+        user = 'uday',
+        date = datetime.utcnow()
+    ))
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title = User('Ivan', 'pavlov'), description = 'Future and Ed collaborated.')
+    return render_template('index.html', title = {'firstname': 'Ivan', 'lastname': 'Pavlov'}, description = 'Future and Ed collaborated.')
 
-@app.route('/add')
+@app.route('/add', methods = ['GET', 'POST'])
 def add():
+    if request.method == "POST":
+        url = request.form['url']
+        store_bookmark(url)
+        app.logger.debug('stored url:' + url)
+        return redirect(url_for('index'))
     return render_template('add.html')
 
 @app.errorhandler(404)
